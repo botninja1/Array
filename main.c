9b283edbf6de25141f2e7482265d4fe2bffa75e2
Marco <71317223+botninja1@users.noreply.github.com>
2024-01-31 19:29:55 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,30 +4,33 @@
 #define SIZE_ARRAY 3
 
 int main(){
-    Array arr = array_init(SIZE_ARRAY, sizeof(int));
-    for(int i=0; i<arr.size; i++){
-        if(!append(&arr, &i)){
+    Array *arr = array_init(SIZE_ARRAY, sizeof(int));
+
+    for(int i=0; i<arr->size; i++){
+        if(!append(arr, &i)){
             fprintf(stderr, "[!] Overflow Append [!]");
             return -1;
         }
     }
-    if(!expand_array(&arr, 1)){
+    if(!expand_array(arr, 1)){
         fprintf(stderr, "[!] Error Expand Array [!]");
         return -1;
     }
     printf("Expand Array:\n");
-    for(int i = 0; i < (arr.index); i++){
-        printf("%d\n", *((int*)get_element(&arr, i)));
+    for(int i = 0; i < (arr->index); i++){
+        printf("%d\n", *((int*)get_element(arr, i)));
     }
-    if(!reduce_array(&arr, 2)){
+    if(!reduce_array(arr, 2)){
         fprintf(stderr, "[!] Error Reduce Array [!]");
         return -1;
     }
 
     printf("Reduce Array:\n");
-    for(int i = 0; i < (arr.index); i++){
-        printf("%d\n", *((int*)get_element(&arr, i)));
+    for(int i = 0; i < (arr->index); i++){
+        printf("%d\n", *((int*)get_element(arr, i)));
     }
-    free_array(&arr);
+    free_array(arr);
+    
+    free(arr);
     return 0;
 }
