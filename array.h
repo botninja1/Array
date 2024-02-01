2af0ef2b96a94d3b7884e5b130f5bea25ef5f47b
Marco <71317223+botninja1@users.noreply.github.com>
2024-02-01 11:55:15 +0100

    Update array.h

--- array.h
+++ array.h
@@ -13,11 +13,16 @@ typedef struct {
 } Array;
 
 Array* array_init(size_t init_size, size_t init_type);
+Array* element_array_init(void *elements, size_t num_elements, size_t element_size);
 bool expand_array(Array *arr, const size_t add_size);
 bool reduce_array(Array *arr, const size_t rm_size);
 void free_array(Array *arr);
 bool append(Array *arr, const void *element);
 void *get_element(Array *arr, const size_t at);
-bool edit_at(Array *arr, const void *element, const size_t element_index);
+bool edit_at(Array *arr, const size_t element_index, const void *element);
+void *begin(Array *arr);
+void *end(Array *arr);
+
+
 
 #endif  // ARRAY_H
