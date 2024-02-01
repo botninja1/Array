28900228e1610b46e3a4c813f8f2d3632bd4abb7
Marco <71317223+botninja1@users.noreply.github.com>
2024-02-01 11:54:37 +0100

    Update array.c

--- array.c
+++ array.c
@@ -18,8 +18,44 @@ Array* array_init(size_t init_size, size_t init_type) {
         .index = 0,
         .ptr = malloc(init_size * init_type),
     };
+    if(!memory->ptr)
+        free(memory);
+        return NULL;
     return memory;
 }
+
+
+/**
+ * Initializes a new Array with the specified elements, number of elements, and element size.
+ * 
+ * @param elements A pointer to the elements to be copied into the Array.
+ * @param num_elements The number of elements in the provided array.
+ * @param element_size The size (in bytes) of each element.
+ * @return A pointer to the newly initialized Array.
+ *         Returns NULL if the number of elements or element size is zero, or if memory allocation fails.
+ */
+Array* element_array_init(void *elements, size_t num_elements, size_t element_size) {
+    if (num_elements == 0 || element_size == 0)
+        return NULL;
+
+    Array *memory = malloc(sizeof(Array));
+    if (!memory) return NULL;
+
+    memory->ptr = malloc(num_elements * element_size);
+    if (!memory->ptr) {
+        free(memory);
+        return NULL;
+    }
+
+    memory->size = num_elements;
+    memory->type = element_size;
+    memory->index = num_elements;
+
+    memcpy(memory->ptr, elements, num_elements * element_size);
+
+    return memory;
+}
+
 
 /*
  * Expand the size of the array by adding a specified number of elements.
@@ -28,7 +64,7 @@ Array* array_init(size_t init_size, size_t init_type) {
  * @return true if the expansion is successful, false otherwise.
  */
 bool expand_array(Array *self, const size_t add_size) {
-    size_t new_size = (add_size + self->size) * self->type;
+     size_t new_size = (add_size + self->size) * self->type;
 
     void *temp_memory = realloc(self->ptr, new_size);
 
@@ -40,6 +76,7 @@ bool expand_array(Array *self, const size_t add_size) {
 
     return false; 
 }
+
 
 /*
  * Reduce the size of the array by removing a specified number of elements.
@@ -76,6 +113,7 @@ void free_array(Array *self) {
     self->size = 0;
 }
 
+
 /*
  * Append an element to the array.
  * @param self Pointer to the Array struct (self-reference).
@@ -95,6 +133,7 @@ bool append(Array *self, const void *element) {
     return true;  
 }
 
+
 /*
  * Retrieve the element at a specific index in the array.
  * @param self Pointer to the Array struct (self-reference).
@@ -108,6 +147,7 @@ void *get_element(Array *self, const size_t at) {
     return ((char *)self->ptr + (at * self->type));
 }
 
+
 /*
  * Edit the element at a specific index in the array.
  * @param self Pointer to the Array struct (self-reference).
@@ -115,7 +155,7 @@ void *get_element(Array *self, const size_t at) {
  * @param element_index Index of the element to edit.
  * @return true if the edit operation is successful, false otherwise.
  */
-bool edit_at(Array *self, const void *element, const size_t element_index) {
+bool edit_at(Array *self, const size_t element_index, const void *element) {
     if ((!self) || (!self->ptr) || ((self->index - 1) < element_index))
         return false;
 
@@ -123,4 +163,26 @@ bool edit_at(Array *self, const void *element, const size_t element_index) {
     memcpy(((char *)self->ptr + offset), element, self->type);
 
     return true; 
+}
+
+
+/**
+ * Returns a pointer to the beginning of the data in the given Array.
+ *
+ * @param self Pointer to the Array.
+ * @return Pointer to the beginning of the data.
+ */
+void *begin(Array *self){
+    return self->ptr;
+}
+
+
+/**
+ * Returns a pointer to the end of the data in the given Array.
+ *
+ * @param self Pointer to the Array.
+ * @return Pointer to the end of the data.
+ */
+void *end(Array *self){
+    return ((char*)self->ptr + ((self->index) * self->type));
 }
