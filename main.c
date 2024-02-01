46d37712af457f5071d13cb7077d3f1240db99dc
Marco <71317223+botninja1@users.noreply.github.com>
2024-02-01 12:05:12 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,35 +2,69 @@
 #include "array.h"
 
 #define SIZE_ARRAY 3
+#define to_int(x) *((int*)(x))  // Cast pointer x to int* and dereference it to get the content
 
-int main(){
+int main() {
+    // Initialize an Array with a specific size and element type
     Array *arr = array_init(SIZE_ARRAY, sizeof(int));
 
-    for(int i=0; i<arr->size; i++){
-        if(!append(arr, &i)){
+    // Populate the Array with elements using the append function
+    for(size_t i = 0; i < arr->size; i++) {
+        if (!append(arr, &i)) {
             fprintf(stderr, "[!] Overflow Append [!]");
             return -1;
         }
     }
-    if(!expand_array(arr, 1)){
+
+    // Expand the size of the Array by 1
+    if (!expand_array(arr, 1)) {
         fprintf(stderr, "[!] Error Expand Array [!]");
         return -1;
     }
+
+    // Print the elements after expansion
     printf("Expand Array:\n");
-    for(int i = 0; i < (arr->index); i++){
-        printf("%d\n", *((int*)get_element(arr, i)));
+    for(int i = 0; i < (arr->index); i++) {
+        printf("arr[%d]: %d\n", i, to_int(get_element(arr, i))); //I use get_element(arr, i) function to show the usage
     }
-    if(!reduce_array(arr, 2)){
+
+    // Reduce the size of the Array by 2
+    if (!reduce_array(arr, 2)) {
         fprintf(stderr, "[!] Error Reduce Array [!]");
         return -1;
     }
-
+    int i = 0;
+    // Print the elements after reduction
     printf("Reduce Array:\n");
-    for(int i = 0; i < (arr->index); i++){
-        printf("%d\n", *((int*)get_element(arr, i)));
+    for(int *it = begin(arr); it != end(arr); it++) {
+        printf("arr[%d]: %d\n", i, *it);
+        i++;
     }
+
+
+    // Edit an element at a specific index
+    int el = 10;
+    size_t index = 0;
+    printf("Edit At:\n");
+    if (edit_at(arr, index, &el)) {
+        printf("arr[%d]: %d\n", index, to_int(get_element(arr, index)));
+    }
+
+    // Initialize a new Array with specified elements
+    int initial_elements[] = {99, 2, 3};
+    Array *buffer = element_array_init(initial_elements, 3, sizeof(int));
+
+    // Iterate over the elements in the buffer Array
+    i = 0;
+    printf("Iterator over the buffer:\n");
+    for(int *it = begin(buffer); it != end(buffer); it++) {
+        printf("arr[%d]: %d\n", i, *it);
+        i++;
+    }
+
+    // Free the memory allocated for both Arrays
     free_array(arr);
-    
     free(arr);
+
     return 0;
 }
